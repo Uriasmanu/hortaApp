3817e558445e926bd31e3311953a1c39ba4b9605
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-12-11 21:51:34 -0300

    a

--- src/componentes/Inicio.tsx
+++ src/componentes/Inicio.tsx
@@ -1,30 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
 
 const Inicio: React.FC = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <View style={styles.container} >
-  
-      <TouchableOpacity style={styles.logo} >
-        <Image source={require('../../assets/topo.png')}  style={styles.logoImage} />
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.logo}>
+        <Image source={require('../../assets/topo.png')} style={styles.logoImage} />
       </TouchableOpacity>
 
-      <TouchableOpacity  style={styles.menu} >
-        <Svg 
-          width={50}
-          height={50}
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="#fff"
-          strokeWidth={2}
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <Path d="M3 12h18M3 6h18M3 18h18" />
-        </Svg>
+      <TouchableOpacity style={styles.menu} onPress={toggleMenu}>
+        {menuOpen ? (
+          <Svg
+            width={45}
+            height={45}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#fff"
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <Path d="M18 6L6 18M6 6l12 12" />
+          </Svg>
+        ) : (
+          <Svg
+            width={45}
+            height={45}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#fff"
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <Path d="M3 12h18M3 6h18M3 18h18" />
+          </Svg>
+        )}
       </TouchableOpacity>
-
     </View>
   );
 };
@@ -32,24 +51,20 @@ const Inicio: React.FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
   logo: {
     flex: 1,
-  
   },
   logoImage: {
     width: 80,
     height: 80,
     marginHorizontal: 12,
   },
-  menu:{
+  menu: {
     justifyContent: 'center',
     marginHorizontal: 16,
   },
-
-
 });
-
 
 export default Inicio;
