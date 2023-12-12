ec5fc27a25c6acd2c03908a5c848cb784dd9784b
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-12-12 12:13:04 -0300

    .

--- src/componentes/Inicio.tsx
+++ src/componentes/Inicio.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import { View, StyleSheet, TouchableOpacity, Image, Text } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
 
-
 import Textos from '../mocks/textos';
 
 const Inicio: React.FC = () => {
@@ -53,7 +52,19 @@ const Inicio: React.FC = () => {
       <View>
         <Text style={styles.titulo}>{Textos.titulo}</Text>
         <TouchableOpacity style={styles.botaoInicio}>
-           <Text style={styles.textoBotoaInicio}>{Textos.saibaMais}</Text>
+           <Text style={styles.textoBotoaInicio}>{Textos.saibaMais}</Text><Svg
+          width={35}
+          height={35}
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#3C533C"
+          strokeWidth={2}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <Path d="M1 12h11M9 7l5 5-4 6" />
+        </Svg>
+           
         </TouchableOpacity>
       </View>
     </View>
@@ -93,11 +104,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#98ff6c',
     marginHorizontal: 12,
     paddingStart: 8,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
     
   },
   textoBotoaInicio:{
     color: '#3C533C',
-    fontSize: 18
+    fontSize: 18,
+    
   }
 });
 
